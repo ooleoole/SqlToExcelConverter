9140cdd10fbd51ddf79c3072103922f4c825deaa
Ola Popp <olapopp85@gmail.com>
2017-07-24 21:14:06 +0200

    done

--- SqlToExelConverter/Program.cs
+++ SqlToExelConverter/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SqlToExelConverter
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("ConnectionString tack");
+            var connectionString = Console.ReadLine();
+            Console.WriteLine("Query tack");
+            var query = Console.ReadLine();
+            Console.WriteLine("Filnamn");
+            var fileName = Console.ReadLine();
+            Console.WriteLine("Snart så");
+            SqlToExcelConverter.Convert(connectionString, query, fileName);
+            Console.WriteLine("Klart! Nu skall du bara hitta filen på data också :)");
+            Console.WriteLine("Tryck på en knapp för att avsluta");
+            Console.ReadKey();
+
         }
     }
 }
---
+++ SqlToExelConverter/SqlToExcelConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using ClosedXML.Excel;
+
+
+namespace SqlToExelConverter
+{
+    public static class SqlToExcelConverter
+    {
+        public static void Convert(string connectionString, string query, string fileName)
+        {
+
+
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+            var command = new SqlCommand(query, connection);
+            var data = new DataSet();
+
+            using (var a = new SqlDataAdapter(command))
+            {
+                a.Fill(data);
+
+                using (XLWorkbook wb = new XLWorkbook())
+                {
+                    wb.Worksheets.Add(data);
+                    wb.SaveAs($"{fileName}.xlsx");
+                    
+                }
+            }
+
+        }
+
+    }
+}
